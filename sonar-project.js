bca93b812467f5d494ca51ff67f1a680ee27bdcd
Ejoh's Engineering Co ltd <104855005+ejohako@users.noreply.github.com>
2022-07-05 09:38:58 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.90.91.88:9000/',
+  serverUrl: 'http://172.31.88.202:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '4638733377dfd43b0baf14e611775c195934e90d',
-	    //'sonar.password': 'admin',
+	    //'sonar.login': '4638733377dfd43b0baf14e611775c195934e90d',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
